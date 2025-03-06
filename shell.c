e3ee347ac6a15a74eefddc097d15563f7cf5e36a
Megan Jen <31997127+meganjen8888@users.noreply.github.com>
2025-03-05 17:31:58 -0800

    Update shell.c
    Correcting code

--- shell.c
+++ shell.c
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <signal.h>
 #define MAX_LINE 80 /* 80 chars per line, per command, should be enough. */
  
 /**
@@ -11,8 +14,16 @@
  * using whitespace as delimiters. setup() sets the args parameter as a
  * null-terminated string.
  */
+
+static char buffer[MAX_LINE];
+void handle_SIGTSTP() {
+    write(STDOUT_FILENO, buffer, strlen(buffer));
+    exit(0); 
+}
 void setup(char inputBuffer[], char *args[],int *background)
 {
+    //FILE *fp = fopen("history.txt", "a");
+    char *yelledWord;
     //introducing all the integer variables
     int length, /* # of characters in the command line */
         i,      /* loop index for accessing inputBuffer array */
@@ -21,7 +32,10 @@ void setup(char inputBuffer[], char *args[],int *background)
    
     ct = 0; //ct variable is called 0
     /* read what the user enters on the command line */
+    memset(inputBuffer,'\0',MAX_LINE);//clear out random characters
     length = read(STDIN_FILENO, inputBuffer, MAX_LINE); 
+    //fprintf(fp, "%s", inputBuffer);
+ 
     //read() reads data previously written to a file. 
     //in this line, STDIN_FILENO is the file descriptor.
     //inputBuffer is what they read data from.
@@ -35,14 +49,30 @@ void setup(char inputBuffer[], char *args[],int *background)
         exit(-1);           /* terminate with error code of -1 */
     }
     /* examine every character in the inputBuffer */
-        if ((strncmp(inputBuffer, "exit", 4))==0) {
-            //printf("test");
-            exit(0); //ok cool
+    if ((strncmp(inputBuffer, "exit", 4))==0) {
+        //remove("history.txt");
+        system("ps -o pid,ppid,pcpu,pmem,etime,user,command");
+        //printf("test");
+        exit(0); //ok cool
+            
     }
+    
     char *temp = malloc(strlen(inputBuffer));
-    if (strncmp("yell", inputBuffer, strlen(inputBuffer))) {
-        printf("%s", strncpy(temp, inputBuffer+5, strlen(inputBuffer)));//now to make it caps
+    //if (strncmp("yell", inputBuffer, strlen(inputBuffer))) {
+    if ((strncmp("yell ", inputBuffer, 5))==0) {
+        yelledWord = strncpy(temp, inputBuffer+5, strlen(inputBuffer));
+        for (int i = 0; i < strlen(yelledWord); i++) {
+            if (yelledWord[i]>='a' && yelledWord[i] <= 'z') {
+                yelledWord[i] = yelledWord[i] - 32;
+            }//nice :)
+        }
+        //printf("test");
+        printf("%s", yelledWord);//now to make it caps
+        printf("\n");
     }
+
+
+
     for (i=0;i<length;i++) {
         switch (inputBuffer[i]){
           case ' ':
@@ -77,26 +107,91 @@ void setup(char inputBuffer[], char *args[],int *background)
      }   
      args[ct] = NULL; /* just in case the input line was > 80 */ 
 }
+
+
 int main(void)
 {
+    FILE *fp;
+    //now we just need to make a text file to put our history in. aka our user inputs
+    int status = 0;
+    pid_t wpid;
     printf("Welcome to mjshell. My pid is %d", getpid());
     printf(".\n");
+    fp = fopen("history.txt", "a");
 char inputBuffer[MAX_LINE];      /* buffer to hold the command entered */
     int background;              /* equals 1 if a command is followed by '&' */
     char *args[(MAX_LINE/2)+1];  /* command line (of 80) has max of 40 arguments */
     int number_of_commands=1;
+    //const char *exeargs[]={inputBuffer, args, background};
+struct sigaction handler;
+    handler.sa_handler = handle_SIGTSTP;
+    handler.sa_flags = SA_RESTART;
+    sigaction(SIGTSTP, &handler, NULL);
+    //while (1) ;
 
     while (1){            /* Program terminates normally inside setup */
        background = 0;
        printf("mjshell[%d", number_of_commands);
        printf("]:\n");//apparently without the \n thing it doesn't print mjshell
-       setup(inputBuffer,args,&background);       /* get next command */
+       setup(inputBuffer,args,&background);
+       fprintf(fp, "%s", inputBuffer);      /* get next command */
+     //  printf("background %d\n", background);
        number_of_commands++;
+
       /* the steps are:
        (0) if built-in command, handle internally
        (1) if not, fork a child process using fork()
        (2) the child process will invoke execvp()
        (3) if background == 0, the parent will wait,
             otherwise returns to the setup() function. */
+    wpid = fork();
+    if (wpid < 0) {
+        fprintf(stderr, "Failed");
+        return 1;
     }
+    else if (wpid == 0) {
+        execvp(inputBuffer, args); //the child
+    }
+    else {
+        printf("[Child pid is ");
+        printf("%d", wpid);
+        if (background==1) {
+        //& turns background flag from 0 to 1
+        // when the background is 1, launch command background by fork()
+        //fflush(stdout);
+        printf(", background = %d\n", background);
+     //   number_of_commands++;
+        //execvp(inputBuffer, args);//prints it twice
+    }
+    else if (background==0) {
+        //waitpid(wpid, &status, 0);
+        //printf("Child process complete\n");
+        wait(NULL);
+        printf(", background = %d\n", background);
+        //printf("Child process complete"); //the parent
+    }
+    }
+    
+ /*           
+    if (!((strncmp(inputBuffer, "exit", 4))==0) && !(strstr(inputBuffer, "yell"))) {
+        //number_of_commands++;
+     
+        //printf("hello");
+
+        
+    } 
+    else {
+
+        number_of_commands++;
+    }
+ */      
+//    printf("%s\n", inputBuffer);
+ /*   if (inputBuffer[strlen(inputBuffer)-1]=='&') {
+        printf("hi");
+    }
+*/
+    }
+
+
+
 }
